8e218b1a06b5aff01d006eb8598c005ac8c38368
James <67014970+JLoweDev@users.noreply.github.com>
2021-03-10 01:06:08 +0000

    botName added to server messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,17 +11,19 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'SocketChat Bot';
+
 // Run when client connects
 io.on('connection', socket => {
     // Emits to user connecting, welcoming them
-    socket.emit('message', 'Welcome to SocketChat!')
+    socket.emit('message', formatMessage(botName, 'Welcome to SocketChat!'))
 
     // Broadcast when a user connects to users other than user
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     })
     
     // Listen for chatMessage
